324c9e979074ee93a31d77b3b9978ff1a94c8999
victor.tomita <victor.tomita@birdie.ai>
2024-05-14 23:01:57 -0300

    fix pathing

--- text_classification/main.py
+++ text_classification/main.py
@@ -6,15 +6,16 @@ env_vars = dotenv_values(".env")
 for key, value in env_vars.items():
     os.environ[key] = value
     
-    
-os.system("python3 image_classification/scripts/0_train_model_over_dataset.py")
+base_path = os.environ['base_path']
 
-os.system("python3 image_classification/scripts/1_data_clustering.py")
+os.system(f"python3 {base_path}/scripts/0_train_model_over_dataset.py")
 
-os.system("python3 image_classification/scripts/2_clustering_based_training.py")
+os.system(f"python3 {base_path}/scripts/1_data_clustering.py")
 
-os.system("python3 image_classification/scripts/3_compare_models.py")
+os.system(f"python3 {base_path}/scripts/2_clustering_based_training.py")
 
-os.system("python3 image_classification/scripts/4_get_embeddings_cluster_models.py")
+os.system(f"python3 {base_path}/scripts/3_compare_models.py")
 
-os.system("python3 image_classification/scripts/5_cst.py")
+os.system(f"python3 {base_path}/scripts/4_get_embeddings_cluster_models.py")
+
+os.system(f"python3 {base_path}/scripts/5_cst.py")
